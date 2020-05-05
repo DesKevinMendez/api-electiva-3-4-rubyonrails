80f8d5922be13c4c5b85979b4a628869ddc25357
Kevin Mendez <kevin_mendez@outlook.com>
2020-05-05 12:16:32 -0600

    Added a personalize error response when dont find an object ActiveRecord

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,13 @@ class ApplicationController < ActionController::API
     def doorkeeper_unauthorized_render_options(error: nil)
         { json: { error: "Not authorized, please, log in or ask an administrator for access" } }
     end
+
+
+    # All the information about the exception is in the parameter: exception
+    rescue_from ActiveRecord::RecordNotFound, with: :show_not_found_errors
+    def show_not_found_errors(exception)
+        render json: {
+            error: exception.message
+        }, status: :not_found
+    end
 end
