cdf9d52dca9c6013df6c5ca7a7acf061163abe85
Kevin Mendez <kevin_mendez@outlook.com>
2020-05-08 12:13:47 -0600

    Validations with dry-validations in contract to Trailblazer was added

--- app/concepts/blogs/contract/create.rb
+++ app/concepts/blogs/contract/create.rb
@@ -1,11 +1,14 @@
 module Blog::Contract
     class Create < Reform::Form
-      
       property :content
       property :title
-  
-      validates :content,  presence: true
-      validates :title, presence: true
+            
+      validation do
+        params do
+          required(:content).filled
+          required(:title).filled
+        end
+      end
 
     end
 end
