8b5fc48ef74a77363057c3aeb880c877a454583f
Kevin Mendez <kevin_mendez@outlook.com>
2020-05-08 12:39:09 -0600

    controller test pass

--- test/controllers/blogs_controller_test.rb
+++ test/controllers/blogs_controller_test.rb
@@ -11,11 +11,13 @@ class BlogsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should create blog" do
-    assert_difference('Blog.count') do
-      post blogs_url, params: { blog: { content: @blog.content, title: @blog.title } }, as: :json
-    end
-
+    post blogs_url, params: {  content: @blog.content, title: @blog.title }, as: :json
     assert_response 201
+  end
+
+  test "should show validation error when created a new blog" do
+    post blogs_url, params: {  content: "", title: "" }, as: :json
+    assert_response 400
   end
 
   test "should show blog" do
@@ -24,15 +26,19 @@ class BlogsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update blog" do
-    patch blog_url(@blog), params: { blog: { content: @blog.content, title: @blog.title } }, as: :json
-    assert_response 200
+    patch blog_url(@blog), params: { content: @blog.content, title: @blog.title }, as: :json
+    assert_response 201
+  end
+
+  test "should show validation error when updated a blog" do
+    patch blog_url(@blog), params: {  content: "", title: "" }, as: :json
+    assert_response 400
   end
 
   test "should destroy blog" do
     assert_difference('Blog.count', -1) do
       delete blog_url(@blog), as: :json
     end
-
     assert_response 204
   end
 end
